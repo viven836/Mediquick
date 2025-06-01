39e5616386286f72a7185f425f2ec82443f74553
viven836 <eeshakarpuradu@gmail.com>
2025-06-01 23:16:03 +0530

    commit

--- backend/app.py
+++ backend/app.py
@@ -334,8 +334,7 @@ def discharge_patient():
         return jsonify({"success": False, "error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
